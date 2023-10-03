2185dff6399bc648b4f8ccc91b01ef9705a2f172
Kim, yongwook <kyw4846@gmail.com>
2023-10-03 16:43:41 +0900

    Main.java 수정

--- src/git_test02/Main.java
+++ src/git_test02/Main.java
@@ -1,8 +1,9 @@
 package git_test02;
-
 public class Main {
-	public static void main(String[] args) {
-		Hello h1 = new Hello();
-		h1.say();
-	}
+
+ 	public static void main(String[] args) {
+ 		Hello hello = new Hello();
+ 		hello.say();
+ 	}
+	
 }
